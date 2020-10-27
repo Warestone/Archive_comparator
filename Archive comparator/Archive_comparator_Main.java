ae67c146ea5de97ffb8f38ba8ac604148a94f9fb
John Warestone <48253995+Warestone@users.noreply.github.com>
2020-10-27 18:11:24 +0300

    Delete Archive_comparator_Main.java

--- Archive comparator/Archive_comparator_Main.java
+++
@@ -1,7 +0,0 @@
-public class Archive_comparator_Main {
-    public static void main(String args[])
-    {
-        Archive_comparator ac = new Archive_comparator();
-        ac.executeCompare();
-    }
-}
